264305797cda1856df9e411d954f7e28a7c3d624
rafaelsanzio <rafaelsanzio27@gmail.com>
2021-04-02 09:28:56 -0300

    feat: creating model User
    feat: creating model User

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,18 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+
+  id?: string;
+  name: string;
+  admin: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export { User };
